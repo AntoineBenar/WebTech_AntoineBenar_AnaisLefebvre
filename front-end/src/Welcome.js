9e7b32b0770b9e50d1b13e59086177389722ad27
AntoineBenar <antoine.benar@yahoo.fr>
2021-12-20 17:35:40 +0100

    New Channel In Back try 1

--- front-end/src/Welcome.js
+++ front-end/src/Welcome.js
@@ -1,7 +1,4 @@
-
-/** @jsxImportSource @emotion/react */
 // Layout
-import { useTheme } from '@mui/styles';
 import { Grid, Typography } from '@mui/material';
 import { ReactComponent as ChannelIcon } from './icons/channel.svg';
 import { ReactComponent as FriendsIcon } from './icons/friends.svg';
@@ -9,6 +6,14 @@ import { ReactComponent as SettingsIcon } from './icons/settings.svg';
 import Popup from 'reactjs-popup';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import 'reactjs-popup/dist/index.css';
+/** @jsxImportSource @emotion/react */
+import {useContext, useRef, useState, useEffect} from 'react';
+import axios from 'axios';
+// Local
+import Context from './Context'
+import { useNavigate, useParams } from 'react-router-dom'
+
+//import "../back-end/lib/db.js" Seems i can't import back end that easily, i have to use exports from b-e
 
 const styles = {
   root: {
@@ -37,6 +42,7 @@ const ChannelForm = () => (
   <>
     <h1>New channel</h1>
     <Formik
+
       initialValues={{ name: "", email: "", acceptedTerms: false }}
       validate={(values) => {
         const errors = {};
@@ -61,7 +67,9 @@ const ChannelForm = () => (
       onSubmit={(values, { setSubmitting }) => {
         // post data to server
 
-        
+      
+
+
 
         alert(JSON.stringify(values, null, 2));
         setSubmitting(false);
@@ -141,45 +149,9 @@ const CreateChannelPopup = () => (
         <div className="header"></div>
         <div className="content">
           <ChannelForm/>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          WARNING, all changes will directly affect the database
         </div>
         <div className="actions">
-          <Popup
-            trigger={
-              <button type="button" className="button">
-                Menu Demo
-              </button>
-            }
-            position="top center"
-            closeOnDocumentClick
-            contentStyle={{ padding: '0px' }}
-            nested
-            keepTooltipInside
-          >
-            <div className="popup-menu">
-              <div className="menu-item"> Menu item 1</div>
-              <div className="menu-item"> Menu item 2</div>
-              <div className="menu-item"> Menu item 3</div>
-              <Popup
-                trigger={<div className="menu-item"> Sup Menu </div>}
-                position="right top"
-                on="hover"
-                closeOnDocumentClick
-                mouseLeaveDelay={300}
-                mouseEnterDelay={0}
-                contentStyle={{ padding: '0px', border: 'none' }}
-                arrow={false}
-                keepTooltipInside
-              >
-                <div className="popup-menu">
-                  <div className="menu-item"> item 1</div>
-                  <div className="menu-item"> item 2</div>
-                  <div className="menu-item"> item 3</div>
-                </div>
-              </Popup>
-              <div className="menu-item"> Menu item 4</div>
-            </div>
-          </Popup>
           <button
             type="button"
             className="button"
@@ -393,6 +365,64 @@ const SettingsPopup = () => (
 
 
 export default function Welcome() {
+
+/*
+
+  const navigate = useNavigate()
+  const { id } = Math.floor(Math.random() * 256);
+  //const {ids} = useParams();
+  const {channels, oauth} = useContext(Context)
+  const channel = channels.find( channel => channel.id === id)
+  const listRef = useRef()
+  const [scrollDown, setScrollDown] = useState(false)
+
+  useEffect( () => {
+   
+    const n=0;
+    while (ids.includes(id)) {
+      id = Math.floor(Math.random() * 256);
+      n = n +1;
+      if(n==1024){
+        navigate('oups');
+      }
+    }
+    const fetch = async () => {
+      try{
+        
+     //   this.post('/channels', async (req, res) => {
+      //    const channel = await this.db.channels.create(req.body)
+      //    res.status(201).json(channel)
+        })
+
+
+        const {data: messages} = await axios.set(`http://localhost:3001/channels/${id}/messages`, {
+          headers: {
+            // TODO: secure the request
+          }
+        })
+        if(listRef.current){
+          listRef.current.scroll()
+        }
+      }catch(err){
+        navigate('/oups')
+      }
+    }
+    fetch()
+  }, [id, oauth, navigate])
+  const onScrollDown = (scrollDown) => {
+    setScrollDown(scrollDown)
+  }
+  const onClickScroll = () => {
+    listRef.current.scroll()
+  }
+  // On refresh, context.channel is not yet initialized
+  if(!channel){
+    return (<div>loading</div>)
+  }
+
+  TRIES TO CREATE A NEW CHANNEL (doesn't work yet)
+*/
+
   //const styles = useStyles(useTheme())
   return (
     
